6aa874c5bc09f604f70f7fd57a9b227e514c5011
danielz02 <31590926+danielz02@users.noreply.github.com>
2022-03-04 17:51:42 -0600

    Implement Lowe's Ratio Test

--- mp2/src/corresponence.py
+++ mp2/src/corresponence.py
@@ -15,25 +15,9 @@ Hint: To avoid using for-loop, check out scipy.spatial.distance.cdist(X,Y,'sqeuc
 import os
 import cv2  # our tested version is 4.5.5
 import numpy as np
+import scipy.spatial.distance
 from matplotlib import pyplot as plt
 from pathlib import Path
-
-basedir = Path('../assets/fountain')
-img1 = cv2.imread(str(basedir / 'images/0000.png'), 0)
-img2 = cv2.imread(str(basedir / 'images/0005.png'), 0)
-
-f, axarr = plt.subplots(2, 1)
-axarr[0].imshow(img1, cmap='gray')
-axarr[1].imshow(img2, cmap='gray')
-plt.show()
-
-# Initiate SIFT detector, The syntax of importing sift descriptor depends on your cv2 version. The following is for
-# cv2 4.5.5,
-sift = cv2.xfeatures2d.SIFT_create()
-
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1, None)  # Hints: kp1 and des1 has the same order. kp1.pt gives the x-y coordinate
-kp2, des2 = sift.detectAndCompute(img2, None)  # Hints: kp2 and des2 has the same order. kp2.pt gives the x-y coordinate
 
 
 def plot_matches(ax_, img1_, img2_, kp_matches_):
@@ -55,45 +39,55 @@ def plot_matches(ax_, img1_, img2_, kp_matches_):
     ax_.axis('off')
 
 
-def select_putative_matches(des1, des2):
+def select_putative_matches(des1_, des2_, ratio_threshold=0.5):
     """
     Arguments:
-        des1: cv2 SIFT descriptors extracted from image1 (None x 128 matrix)
-        des2: cv2 SIFT descriptors extracted from image2 (None x 128 matrix)
+        :param des1_: cv2 SIFT descriptors extracted from image1 (None x 128 matrix)
+        :param des2_: cv2 SIFT descriptors extracted from image2 (None x 128 matrix)
+        :param ratio_threshold: threshold for Lowe's ratio test
     Returns:
         matches: List of tuples. Each tuple characterizes a match between descriptor in des1 and descriptor in des2. 
                  The first item in each tuple stores the descriptor index in des1, and the second stores index in des2.
                  For example, returning a tuple (8,10) means des1[8] and des2[10] are a match.
     """
-    matches = []
 
-    # --------------------------- Begin your code here ---------------------------------------------
+    dists = scipy.spatial.distance.cdist(des1_, des2_, "sqeuclidean")
+    dist_ranks = dists.argsort(axis=0)
+    fc = np.argwhere(dist_ranks == 0)
+    fs = np.argwhere(dist_ranks == 1)
+    dist_ratios = dists[fc[:, 0], fc[:, 1]] / dists[fs[:, 0], fs[:, 1]]
+    filtered_matches = [tuple(x) for x in fc[dist_ratios < ratio_threshold]]
 
-    # --------------------------- End your code here   ---------------------------------------------
-
-    def lowe_ratio_test(matches):
-        """
-        run lowe ratio test to filter out 
-        Arguments: 
-            matches: output from select_putative_matches function
-        Returns:
-            matches: filter matches using lowe ratio test
-        """
-        # --------------------------- Begin your code here ---------------------------------------------
-
-        # --------------------------- End your code here   ---------------------------------------------
-        return matches
-
-    filtered_matches = lowe_ratio_test(matches)
     return filtered_matches
 
 
-matches = select_putative_matches(des1, des2)  # your code
-match_indices = np.random.permutation(len(matches))[:20]  # you can change it to visualize more points
-kp_matches = np.array(
-    [[kp1[i].pt[0], kp1[i].pt[1], kp2[j].pt[0], kp2[j].pt[1]] for i, j in np.array(matches)[match_indices]])
+if __name__ == "__main__":
+    basedir = Path('../assets/fountain')
+    img1 = cv2.imread(str(basedir / 'images/0000.png'), 0)
+    img2 = cv2.imread(str(basedir / 'images/0005.png'), 0)
 
-fig, ax = plt.subplots(figsize=(10, 5))
-plot_matches(ax, img1, img2, kp_matches)
-ax.set_title('Matches visualization')
-plt.show()
+    f, axarr = plt.subplots(2, 1)
+    axarr[0].imshow(img1, cmap='gray')
+    axarr[1].imshow(img2, cmap='gray')
+    plt.show()
+
+    # Initiate SIFT detector, The syntax of importing sift descriptor depends on your cv2 version. The following is for
+    # cv2 4.5.5,
+    sift = cv2.SIFT_create()
+
+    # find the keypoints and descriptors with SIFT
+    # Hints: kp1 and des1 has the same order. kp1.pt gives the x-y coordinate
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    # Hints: kp2 and des2 has the same order. kp2.pt gives the x-y coordinate
+    kp2, des2 = sift.detectAndCompute(img2, None)
+
+    matches = select_putative_matches(des1, des2)  # your code
+    match_indices = np.random.permutation(len(matches))[:20]  # you can change it to visualize more points
+    kp_matches = np.array(
+        [[kp1[i].pt[0], kp1[i].pt[1], kp2[j].pt[0], kp2[j].pt[1]] for i, j in np.array(matches)[match_indices]]
+    )
+
+    fig, ax = plt.subplots(figsize=(10, 5))
+    plot_matches(ax, img1, img2, kp_matches)
+    ax.set_title('Matches visualization')
+    plt.show()

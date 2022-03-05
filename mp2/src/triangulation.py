65fe777b03f9559ae0222b5c613bd2d40aba2115
danielz02 <31590926+danielz02@users.noreply.github.com>
2022-03-05 02:59:54 -0600

    Implement linear triangulation and visualization

--- mp2/src/triangulation.py
+++ mp2/src/triangulation.py
@@ -13,51 +13,78 @@ from matplotlib import pyplot as plt
 from math import sqrt
 import random
 
-# Coords of matched keypoint pairs in image 1 and 2, dims (#matches, 4). Same pair of images as before
-# For each row, it consists (k1_x, k1_y, k2_x, k2_y).
-# If necessary, you can convert float to int to get the integer coordinate
-all_good_matches = np.load('../assets/all_good_matches.npy')
 
-K1 = np.load('../assets/fountain/Ks/0000.npy')
-K2 = np.load('../assets/fountain/Ks/0005.npy')
-
-R1 = np.load('../assets/fountain/Rs/0000.npy')
-R2 = np.load('../assets/fountain/Rs/0005.npy')
-
-t1 = np.load('../assets/fountain/ts/0000.npy')
-t2 = np.load('../assets/fountain/ts/0005.npy')
-
-
-def triangulate(K1, K2, R1, R2, t1, t2, all_good_matches):
+def triangulate(k1, k2, r1, r2, t1, t2, matches):
     """
     Arguments:
-        K1: intrinsic matrix for image 1, dim: (3, 3)
-        K2: intrinsic matrix for image 2, dim: (3, 3)
-        R1: rotation matrix for image 1, dim: (3, 3)
-        R2: rotation matrix for image 1, dim: (3, 3)
+        k1: intrinsic matrix for image 1, dim: (3, 3)
+        k2: intrinsic matrix for image 2, dim: (3, 3)
+        r1: rotation matrix for image 1, dim: (3, 3)
+        r2: rotation matrix for image 1, dim: (3, 3)
         t1: translation for image 1, dim: (3,)
         t2: translation for image 1, dim: (3,)
-        all_good_matches:  dim: (#matches, 4)
+        matches:  dim: (#matches, 4)
     Returns:
         points_3d, dim: (#matches, 3)
     """
-    points_3d = None
     # --------------------------- Begin your code here ---------------------------------------------
+    pts_3d = []
+    n, *_ = matches.shape
+    p1 = k1 @ np.hstack([r1, t1.reshape(-1, 1)])
+    p2 = k2 @ np.hstack([r2, t2.reshape(-1, 1)])
 
+    for i in range(n):
+        x, y, x_prime, y_prime = matches[i]
+        a = np.vstack([
+            y * p1[2] - p1[1],
+            p1[0] - x * p1[2],
+            y_prime * p2[2] - p2[1],
+            p2[0] - x_prime * p2[2]
+        ])
+        *_, vh = np.linalg.svd(a, full_matrices=True)
+        pts_3d += [vh[-1]]
+    pts_3d = np.stack(pts_3d, axis=0)
+    pts_3d = pts_3d[:, :3] / pts_3d[:, 3].reshape(-1, 1)
     # --------------------------- End your code here   ---------------------------------------------
-    return points_3d
+    return pts_3d
 
 
-points_3d = triangulate(K1, K2, R1, R2, t1, t2, all_good_matches)
-if points_3d is not None:
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(points_3d)
+if __name__ == "__main__":
+    # Coords of matched keypoint pairs in image 1 and 2, dims (#matches, 4). Same pair of images as before
+    # For each row, it consists (k1_x, k1_y, k2_x, k2_y).
+    # If necessary, you can convert float to int to get the integer coordinate
+    all_good_matches = np.load('../assets/all_good_matches.npy')
 
-    # Visualize both point and camera Check this link for Open3D visualizer
-    # http://www.open3d.org/docs/release/tutorial/visualization/visualization.html#Function-draw_geometries Check
-    # this function for adding a virtual camera in the visualizer
-    # http://www.open3d.org/docs/release/tutorial/visualization/visualization.html#Function-draw_geometries Open3D is
-    # not the only option. You could use matplotlib, vtk or other visualization tools as well.
-    # --------------------------- Begin your code here ---------------------------------------------
+    K1 = np.load('../assets/fountain/Ks/0000.npy')
+    K2 = np.load('../assets/fountain/Ks/0005.npy')
 
-    # --------------------------- End your code here   ---------------------------------------------
+    R1 = np.load('../assets/fountain/Rs/0000.npy')
+    R2 = np.load('../assets/fountain/Rs/0005.npy')
+
+    T1 = np.load('../assets/fountain/ts/0000.npy')
+    T2 = np.load('../assets/fountain/ts/0005.npy')
+
+    points_3d = triangulate(K1, K2, R1, R2, T1, T2, all_good_matches)
+    if points_3d is not None:
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(points_3d)
+
+        # Visualize both point and camera
+        # Check this link for Open3D visualizer
+        # http://www.open3d.org/docs/release/tutorial/visualization/visualization.html#Function-draw_geometries
+        # Check this function for adding a virtual camera in the visualizer
+        # Open3D is not the only option. You could use matplotlib, vtk or other visualization tools as well.
+        # --------------------------- Begin your code here ---------------------------------------------
+
+        extrinsic = np.eye(4)
+        extrinsic[:3, :3] = R1
+        extrinsic[:3, -1] = T1.reshape(-1)
+        cam1 = o3d.geometry.LineSet().create_camera_visualization(1520, 1006, K1, extrinsic)
+
+        extrinsic = np.eye(4)
+        extrinsic[:3, :3] = R2
+        extrinsic[:3, -1] = T2.reshape(-1)
+        cam2 = o3d.geometry.LineSet().create_camera_visualization(1520, 1006, K2, extrinsic)
+
+        o3d.visualization.draw_geometries([pcd, cam1, cam2])
+        # --------------------------- End your code here   ---------------------------------------------

9e877abd96fdcea7954b6d09c436acd8962f0662
danielz02 <31590926+danielz02@users.noreply.github.com>
2022-03-24 16:48:45 -0500

    Refactor odometry estimation

--- mp3/src/odometry.py
+++ mp3/src/odometry.py
@@ -1,76 +1,76 @@
 from re import I
 import numpy as np
 import open3d as o3d
-from icp_sol import read_data, icp, rgbd2pts
+from icp import read_data, icp, rgbd2pts
 
 
 def odometry_error(T_W2Cs_pred, T_W2Cs_gt):
-  rot_err = 0
-  trans_err = 0
-  return rot_err, trans_err
+    rot_err = 0
+    trans_err = 0
+    return rot_err, trans_err
+
 
 color_im, depth_im, K, T_init = read_data(0)
 pcd_init = rgbd2pts(color_im, depth_im, K)
 pcd_down = pcd_init.voxel_down_sample(voxel_size=0.02)
 h, w, c = color_im.shape
 
-step = 10 # try different step
+step = 10  # try different step
 end = 100
-cam_init = o3d.geometry.LineSet.create_camera_visualization(w, h, K, np.eye(4), scale = 0.1)
-gt_cam = o3d.geometry.LineSet.create_camera_visualization(w, h, K, np.eye(4), scale = 0.1)
+cam_init = o3d.geometry.LineSet.create_camera_visualization(w, h, K, np.eye(4), scale=0.1)
+gt_cam = o3d.geometry.LineSet.create_camera_visualization(w, h, K, np.eye(4), scale=0.1)
 gt_cam.paint_uniform_color((1, 0, 0))
 
 pred_poses = {}
 gt_poses = {}
 pcds = {}
-pred_poses[0] = np.eye(4) # assuming first frame is global coordinate center
-gt_poses[0] = np.eye(4) # assuming first frame is global coordinate center
-pcds[0] = pcd_down # save point cloud for init frame
+pred_poses[0] = np.eye(4)  # assuming first frame is global coordinate center
+gt_poses[0] = np.eye(4)  # assuming first frame is global coordinate center
+pcds[0] = pcd_down  # save point cloud for init frame
 vis_list = [pcd_init, cam_init, gt_cam]
 
 for frame in range(step, end, step):
-  color_im, depth_im, K, T_tgt = read_data(frame - step)
-  target = rgbd2pts(color_im, depth_im, K)
-  color_im, depth_im, K, T_src = read_data(frame)
-  source = rgbd2pts(color_im, depth_im, K)
+    color_im, depth_im, K, T_tgt = read_data(frame - step)
+    target = rgbd2pts(color_im, depth_im, K)
+    color_im, depth_im, K, T_src = read_data(frame)
+    source = rgbd2pts(color_im, depth_im, K)
 
-  # some pre-processing, including computing normals and downsampling
-  source_down = source.voxel_down_sample(voxel_size=0.02)
-  target_down = target.voxel_down_sample(voxel_size=0.02)
-  source.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
-  target.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+    # some pre-processing, including computing normals and downsampling
+    source_down = source.voxel_down_sample(voxel_size=0.02)
+    target_down = target.voxel_down_sample(voxel_size=0.02)
+    source.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+    target.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
 
-  # Question 6 --- Could you update the camera poses using ICP?
-  # Hint1: you could call your ICP to estimate relative pose betwen frame t and t-step
-  # Hint2: based on the estimated transform between frame t and t-step, are you able to compute world to camera transform for t?
-  
-  # Your code
-  # ------------------------
-  T_W2C = np.eye(4)  # world to current camera frame
-  
-  
-  pred_poses[frame] = T_W2C
-  # ------------------------
-  
-  # get ground-truth pose
-  T_W2C_gt =  np.linalg.inv(T_src) @ T_init # world to init to current
-  gt_poses[frame] = T_W2C_gt # ground truth
-  pcds[frame] = source_down
+    # Question 6 --- Could you update the camera poses using ICP? Hint1: you could call your ICP to estimate relative
+    # pose betwen frame t and t-step Hint2: based on the estimated transform between frame t and t-step, are you able
+    # to compute world to camera transform for t?
 
-  current_cam = o3d.geometry.LineSet.create_camera_visualization(w, h, K, T_W2C, scale = 0.1)
-  gt_cam = o3d.geometry.LineSet.create_camera_visualization(w, h, K, T_W2C_gt, scale = 0.1)
-  gt_cam.paint_uniform_color((1, 0, 0))
-  source.transform(np.linalg.inv(T_W2C))
-  vis_list.append(source)
-  vis_list.append(current_cam)
-  vis_list.append(gt_cam)
-  print("Frame %d is done" % frame)
+    # Your code
+    # ------------------------
+    T_W2C = np.eye(4)  # world to current camera frame
+
+    pred_poses[frame] = T_W2C
+    # ------------------------
+
+    # get ground-truth pose
+    T_W2C_gt = np.linalg.inv(T_src) @ T_init  # world to init to current
+    gt_poses[frame] = T_W2C_gt  # ground truth
+    pcds[frame] = source_down
+
+    current_cam = o3d.geometry.LineSet.create_camera_visualization(w, h, K, T_W2C, scale=0.1)
+    gt_cam = o3d.geometry.LineSet.create_camera_visualization(w, h, K, T_W2C_gt, scale=0.1)
+    gt_cam.paint_uniform_color((1, 0, 0))
+    source.transform(np.linalg.inv(T_W2C))
+    vis_list.append(source)
+    vis_list.append(current_cam)
+    vis_list.append(gt_cam)
+    print("Frame %d is done" % frame)
 
 o3d.visualization.draw_geometries(vis_list,
                                   zoom=0.422,
-                                  front = [ -0.11651295252277051, -0.047982289143896774, -0.99202945108647766 ],
-                                  lookat = [ 0.023592929264511786, 0.051808635289583765, 1.7903649529102956 ],
-                                  up = [ 0.097655832648056065, -0.9860023571949631, -0.13513952033284915 ])
+                                  front=[-0.11651295252277051, -0.047982289143896774, -0.99202945108647766],
+                                  lookat=[0.023592929264511786, 0.051808635289583765, 1.7903649529102956],
+                                  up=[0.097655832648056065, -0.9860023571949631, -0.13513952033284915])
 
 # Question 7: Relative Trajectory Error
 # implement the relative rotation error and relative translation error
@@ -83,11 +83,11 @@ o3d.visualization.draw_geometries(vis_list,
 
 # ------------------------
 
-# Question 8: Pose graph optimization
-# Now we have an simple odometry solution, where each frame's pose in world coordinate is decided by its previous frame's pose and a relative transformation provided by ICP.
-# Given the pose T_0 and T_40, please validate whether the relative transformation
-# caculated from the two odometry poses will perfectly agree with the transformation we estimated from ICP?
-# If not, explain why. If yes, explain why (in your pdf).
+# Question 8: Pose graph optimization Now we have an simple odometry solution, where each frame's pose in world
+# coordinate is decided by its previous frame's pose and a relative transformation provided by ICP. Given the pose
+# T_0 and T_40, please validate whether the relative transformation caculated from the two odometry poses will
+# perfectly agree with the transformation we estimated from ICP? If not, explain why. If yes, explain why (in your
+# pdf).
 
 color_im, depth_im, K, T_tgt = read_data(0)
 target = rgbd2pts(color_im, depth_im, K)
@@ -106,12 +106,11 @@ T_40 = pred_poses[40]
 print("Relative transfrom from ICP:", np.linalg.inv(final_Ts[-1]))
 print("Relative transfrom from odometry:", T_40)
 
-# Question 8: to ensure the consistency, we could build a pose graph to further improve the performance.
-# Each node is the a camera pose
-# Each edge will describe the relative transformation between the node, provided by ICP.
-# The general idea of pose graph optimization is to jointly optimize the pose such that maximum consensus has been reached:
-# argmin_{T_i} \sum_i^N \sum_{j>i}^{i+K} (T_ij - inv(T_j) @ T_i)^2
-# where T_ij is transformation from i to j, T_i is transformation from i to world (global coordinate)
+# Question 8: to ensure the consistency, we could build a pose graph to further improve the performance. Each node is
+# the a camera pose Each edge will describe the relative transformation between the node, provided by ICP. The
+# general idea of pose graph optimization is to jointly optimize the pose such that maximum consensus has been
+# reached: argmin_{T_i} \sum_i^N \sum_{j>i}^{i+K} (T_ij - inv(T_j) @ T_i)^2 where T_ij is transformation from i to j,
+# T_i is transformation from i to world (global coordinate)
 
 # In this question, you are going to leverage pose graph optimization to build dense pose graph.
 # A node will be connected if their difference in frame number is smaller or equal to 30
@@ -138,11 +137,11 @@ for point_id in range(len(pcds)):
     point_frame = frame_list[point_id]
     pcds[point_frame].transform(pose_graph.nodes[point_id].pose)
     T_C2W = pose_graph.nodes[point_id].pose
-    pgo_cam = o3d.geometry.LineSet.create_camera_visualization(w, h, K, np.linalg.inv(T_C2W), scale = 0.1)
+    pgo_cam = o3d.geometry.LineSet.create_camera_visualization(w, h, K, np.linalg.inv(T_C2W), scale=0.1)
     pgo_cam.paint_uniform_color((0, 1, 0))
-    gt_cam = o3d.geometry.LineSet.create_camera_visualization(w, h, K, gt_poses[point_frame], scale = 0.1)
+    gt_cam = o3d.geometry.LineSet.create_camera_visualization(w, h, K, gt_poses[point_frame], scale=0.1)
     gt_cam.paint_uniform_color((1, 0, 0))
-    odometry_cam = o3d.geometry.LineSet.create_camera_visualization(w, h, K, pred_poses[point_frame], scale = 0.1)
+    odometry_cam = o3d.geometry.LineSet.create_camera_visualization(w, h, K, pred_poses[point_frame], scale=0.1)
     odometry_cam.paint_uniform_color((0, 0, 1))
     vis_list.append(pgo_cam)
     vis_list.append(odometry_cam)
@@ -151,6 +150,6 @@ for point_id in range(len(pcds)):
 
 o3d.visualization.draw_geometries(vis_list,
                                   zoom=0.422,
-                                  front = [ -0.11651295252277051, -0.047982289143896774, -0.99202945108647766 ],
-                                  lookat = [ 0.023592929264511786, 0.051808635289583765, 1.7903649529102956 ],
-                                  up = [ 0.097655832648056065, -0.9860023571949631, -0.13513952033284915 ])
+                                  front=[-0.11651295252277051, -0.047982289143896774, -0.99202945108647766],
+                                  lookat=[0.023592929264511786, 0.051808635289583765, 1.7903649529102956],
+                                  up=[0.097655832648056065, -0.9860023571949631, -0.13513952033284915])
